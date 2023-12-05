8dab81c7d21ca1e505e178822772d8d64bd90385
Romnick Sarmiento <135184315+s4rmier@users.noreply.github.com>
2023-12-05 18:54:05 +0800

    img support

--- events/models.py
+++ events/models.py
@@ -2,15 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Event(models.Model):
-    title = models.CharField(max_length=28)
-    description = models.TextField(max_length=256)
-    address = models.CharField(max_length=64)
-    time = models.TimeField(default='00:00') 
-    date = models.DateField() 
+    eventTitle = models.CharField(max_length=28)
+    eventDescription = models.TextField(max_length=256)
+    eventAddress = models.CharField(max_length=64)
+    eventTime = models.TimeField(default='00:00') 
+    eventDate = models.DateField() 
 
     def __str__(self):
-        return self.title
+        return self.eventTitle
     
 class EventImage(models.Model):
     event = models.ForeignKey(Event, related_name="images", on_delete=models.CASCADE)
     image = models.ImageField(upload_to="event_images/")
+    imgtype = models.IntegerField(blank=True, null=True)
